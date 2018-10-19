1814bc7ceb1947d2d3ef569fb1bc0adec457b9ff
Metacursor <contact@metacursor.com>
2018-10-19 13:40:33 +0800

    Testing: more pythonic maybe?

--- gsmmodem/modem.py
+++ gsmmodem/modem.py
@@ -1423,7 +1423,7 @@ class GsmModem(SerialComms):
         """
         # Switch to the correct memory type if required
         self._setSmsMemory(readDelete=memory)
-        self.write('AT+CMGF=0')
+        self.smsTextMode = False
         msgData = self.write('AT+CMGR={0}'.format(index))
         # Parse meta information
         if self.smsTextMode:
